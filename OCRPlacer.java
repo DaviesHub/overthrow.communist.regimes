72da8ea5bbf21b0c4d27fcc2fa5956c8455cf318
DaviesHub <davieschuks.a@gmail.com>
2022-10-05 17:42:04 +0200

    Add game helper class

---
+++ OCRPlacer.java
@@ -0,0 +1,58 @@
+package overthrow.communist.regimes;
+
+import java.util.ArrayList;
+
+public class OCRPlacer {
+    public class CRPlacer {
+        private static final String alphabet = "abcdefg";
+        private int gridLength = 7;
+        private int gridSize = 49;
+        private int[] grid = new int[gridSize];
+        private int crCount = 0;
+
+        public ArrayList<String> placeCR(int crSize) {
+            ArrayList<String> alphaCells = new ArrayList<String>();
+            String temp = null;
+            int[] coords = new int[crSize];
+            int attempts = 0;
+            boolean success = false;
+            int location = 0;
+            crCount++;
+            int incr = 1;
+            if ((crCount % 2) == 1) {
+                incr = gridLength;
+            }
+            while (!success & attempts++ < 200) {
+                location = (int) (Math.random() * gridSize);
+                int x = 0;
+                success = true;
+                while (success && x < crSize) {
+                    if (grid[location] == 0) {
+                        coords[x++] = location;
+                        location += incr;
+                    }
+                    if (location >= gridSize) {
+                        success = false;
+                    }
+                    if (x > 0 && (location % gridLength == 0)) {
+                        success = false;
+                    } else {
+                        success = false;
+                    }
+                }
+            }
+            int x = 0;
+            int row = 0;
+            int column = 0;
+            while (x < crSize) {
+                grid[coords[x]] = 1;
+                row = (int) (coords[x] / gridLength);
+                column = coords[x] % gridLength;
+                temp = String.valueOf(alphabet.charAt(column));
+                alphaCells.add(temp.concat(Integer.toString(row)));
+                x++;
+            }
+            return alphaCells;
+        }
+    }
+}
