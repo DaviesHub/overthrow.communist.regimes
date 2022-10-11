8221a3efc85d0de9a75c215d487bd1c02440abb6
DaviesHub <davieschuks.a@gmail.com>
2022-10-11 21:22:53 +0200

    Edit startPlaying method to end game if isAlive is false

--- OCRGame.java
+++ OCRGame.java
@@ -9,9 +9,19 @@ class CommunistRegime {
      */
     private ArrayList<String> locationCells;
     private int numOfHits;
+    private String name;
 
     public void setLocationCells(ArrayList<String> loc) {
+
         locationCells = loc;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public String checkYourself(String userInput) {
@@ -29,6 +39,7 @@ class CommunistRegime {
 
         return result;
     }
+
 }
 public class OCRGame {
 
@@ -57,11 +68,45 @@ public class OCRGame {
 
             comRegime.setLocationCells(crCells);
         }
+        System.out.println("In this game, you will guess the locations of totalitarian communist regimes");
+        System.out.println("Take them all out (Well, assuming you are a capitalist.)");
+        System.out.println("The communist regimes are USSR, CCP, PCV. Try to overthrow them with the fewest number of guesses.")
     }
 
     public void startPlaying() {
+        boolean isAlive = true;
+        while (isAlive) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter a cell coordinate (A1 to G7): ");
+            String userGuess = scanner.next();
 
+            for (CommunistRegime comRegime : comRegimeList) {
+                String result = comRegime.checkYourself(userGuess);
+                }
+            if (comRegimeList.isEmpty()) {
+                isAlive = false;
+            }
+        }
     }
+    public String checkUserGuess(String userGuess) {
+        numGuesses++;
+        String result = "Miss";
+        for (CommunistRegime comRegime : comRegimeList) {
+            result = comRegime.checkYourself(userGuess);
+            if (result.equals("Kill")) {
+                System.out.println("Congratulations! You have overthrown " + comRegime.getName() + " Regime.");
+                comRegimeList.remove(comRegime);
+            }
+        }
+        System.out.println(result);
+        if (comRegimeList.isEmpty()) {
+            isAlive;
+        }
+        System.out.println(result);
+
+        return result;
+    }
+
     public static void main(String[] args) {
         int numOfGuesses = 0;
         CommunistRegime ocr = new CommunistRegime();
